2f1ee36708c17532b48242c869fba879b7f5b8f5
Fritz Windisch <friwidev@gmail.com>
2023-08-24 03:51:14 +0200

    Compile twice to resolve references etc

--- thesis/compile.sh
+++ thesis/compile.sh
@@ -2,4 +2,5 @@
 
 cd "$(dirname "$0")" || exit 1
 latexmk -auxdir=tmp -outdir=tmp -pdflatex='texfot lualatex -interaction nonstopmode' -pdf main.tex
+latexmk -auxdir=tmp -outdir=tmp -pdflatex='texfot lualatex -interaction nonstopmode' -pdf main.tex
 mv tmp/main.pdf .

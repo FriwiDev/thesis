9295a3bf84fb5e9f72e7ffe1d912c489ed79a11d
Fritz Windisch <friwidev@gmail.com>
2023-09-06 14:56:23 +0200

    Work on comments, fix spelling mistakes

--- thesis/compile.sh
+++ thesis/compile.sh
@@ -2,5 +2,6 @@
 
 cd "$(dirname "$0")" || exit 1
 latexmk -auxdir=tmp -outdir=tmp -pdflatex='texfot lualatex -interaction nonstopmode' -pdf main.tex
+makeglossaries main
 latexmk -auxdir=tmp -outdir=tmp -pdflatex='texfot lualatex -interaction nonstopmode' -pdf main.tex
 mv tmp/main.pdf .

20f97176f4fe8370097e6241bf85dcbe426e82fd
Fritz Windisch <friwidev@gmail.com>
2023-09-07 23:19:09 +0200

    Small fixes

--- thesis/compile.sh
+++ thesis/compile.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd "$(dirname "$0")" || exit 1
+echo ">>> First render"
 latexmk -auxdir=tmp -outdir=tmp -pdflatex='texfot lualatex -interaction nonstopmode' -pdf main.tex
-makeglossaries main
+echo ">>> Make glossaries"
+makeglossaries -d tmp main
+echo ">>> Second render"
 latexmk -auxdir=tmp -outdir=tmp -pdflatex='texfot lualatex -interaction nonstopmode' -pdf main.tex
 mv tmp/main.pdf .

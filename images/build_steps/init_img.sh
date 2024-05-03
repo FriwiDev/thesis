bb37e67bd5e2bed8a64c869e223b962eec8a1fc8
Fritz Windisch <friwidev@gmail.com>
2024-05-03 14:05:13 +0200

    Fix images building using nat instead of macvlan

--- images/build_steps/init_img.sh
+++ images/build_steps/init_img.sh
@@ -4,7 +4,13 @@ rm -f "export/$1.tar.gz"
 lxc rm -f "$1"
 lxc image delete "$1"
 lxc remote add --protocol simplestreams ubuntu-minimal https://cloud-images.ubuntu.com/minimal/releases/ || true
-lxc init ubuntu-minimal:focal "$1" --profile default --profile macvlan
+
+lxc network create lxcbr0
+lxc network set lxcbr0 ipv4.nat true
+lxc network set lxcbr0 ipv4.nat.address 172.23.219.44
+
+lxc init ubuntu-minimal:focal "$1" --profile default
+lxc network attach lxcbr0 "$1" lxcbr0 eth0
 lxc start "$1"
 sleep 3 #Allow container to perform dhcp and establish a connection
 lxc exec "$1" -- apt update
